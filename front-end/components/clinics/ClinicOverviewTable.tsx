2c87c3a3b36b2330d6ff58018c2f98d45c4f7fc9
Irina Lazar <irina.lazar@student.ucll.be>
2024-12-18 23:41:28 +0200

    list of doctors on clinic click

--- front-end/components/clinics/ClinicOverviewTable.tsx
+++ front-end/components/clinics/ClinicOverviewTable.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Clinic } from '@types';
+import { Clinic, Doctor } from '@types';
 import { mutate } from 'swr';
 
 type Props = {
@@ -7,6 +7,11 @@ type Props = {
 };
 
 const ClinicOverviewTable: React.FC<Props> = ({ clinics }: Props) => {
+  const [selectedClinic, setSelectedClinic] = useState<Clinic | null>(null);
+
+  const handleClinicClick = (clinic: Clinic) => {
+    setSelectedClinic(clinic);
+  };
 
   return (
     <>
@@ -21,7 +26,7 @@ const ClinicOverviewTable: React.FC<Props> = ({ clinics }: Props) => {
           </thead>
           <tbody>
           {clinics.map((clinic: Clinic) => (
-          <tr key={clinic.id}>
+          <tr key={clinic.id} onClick={() => handleClinicClick(clinic)} style={{ cursor: 'pointer' }}>
             <td>{clinic.address}</td>
             <td>{clinic.contactNumber}</td>
             <td>{clinic.rating}</td>
@@ -29,6 +34,31 @@ const ClinicOverviewTable: React.FC<Props> = ({ clinics }: Props) => {
         ))}
           </tbody>
         </table>
+      )}
+      {selectedClinic && (
+        <div>
+          <h3>Doctors at {selectedClinic.address}</h3>
+          {selectedClinic.doctors.length > 0 ? (
+            <table className="table table-hover">
+              <thead>
+                <tr>
+                  <th scope="col">Name</th>
+                  <th scope="col">Department</th>
+                </tr>
+              </thead>
+              <tbody>
+                {selectedClinic.doctors.map((doctor: Doctor) => (
+                  <tr key={doctor.id}>
+                    <td>{doctor.user.firstName} {doctor.user.lastName}</td>
+                    <td>{doctor.department}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          ) : (
+            <p>No doctors found for this clinic.</p>
+          )}
+        </div>
       )}
     </>
   );
