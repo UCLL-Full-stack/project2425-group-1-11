49079d647f4bcbda2a7820102c6b506faf4512ce
Irina Lazar <irina.lazar@student.ucll.be>
2024-12-18 23:52:19 +0200

    added users list on homepage

--- front-end/pages/index.tsx
+++ front-end/pages/index.tsx
@@ -6,7 +6,15 @@ import useCurrentUserId from 'hook/useCurrentUserId';
 
 const Home: React.FC = () => {
 
-  const userId = useCurrentUserId()
+  const userId = useCurrentUserId();
+
+  const users = [
+    { username: 'irinalazar', password: 'irina8', role: 'patient' },
+    { username: 'furquanmobeen', password: 'furquan12', role: 'patient' },
+    { username: 'eddyndacasaba', password: 'eddy14', role: 'doctor' },
+    { username: 'yonghoonkim', password: 'kim10', role: 'doctor' },
+    { username: 'naphatpruekveeraparb', password: 'naphat420', role: 'admin' },
+  ];
 
   return (
     <>
@@ -33,6 +41,26 @@ const Home: React.FC = () => {
             The medical appointments app lets you manage your appointments as a user. <br />
             You can also choose your doctor and manage your medical records.
           </p>
+        </div>
+        <div>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">Username</th>
+                <th scope="col">Password</th>
+                <th scope="col">Role</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <tr key={index}>
+                  <td>{user.username}</td>
+                  <td>{user.password}</td>
+                  <td>{user.role}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </main>
     </>
