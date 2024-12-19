8e1e8830cb2033c27a650c48e892c110ee192587
Furquan Mobeen <furquan.mobeen@student.ucll.be>
2024-12-19 12:39:56 +0100

    Made a get doctor by id request in the backend

--- back-end/controller/doctor.routes.ts
+++ back-end/controller/doctor.routes.ts
@@ -46,4 +46,38 @@ doctorRouter.get('/', async (req: Request, res: Response) => {
     }
 });
 
+/**
+ * @swagger
+ * /doctors/{id}:
+ *   get:
+ *     tags:
+ *       - Doctor
+ *     summary: Get a list of all doctors.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *         description: The doctor ID
+ *     description: Returns an array of doctors. Each item in the array is of type Doctor.
+ *     responses:
+ *       200:
+ *         description: A JSON array of doctor objects.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Doctor'
+ */
+doctorRouter.get('/:id', async (req: Request, res: Response) => {
+    try {
+        const doctors = await doctorService.getDoctorById({ id: Number(req.params.id) });
+        res.status(200).json(doctors);
+    } catch (error) {
+        res.status(400).json({status: 'error', errorMessage: (error as Error).message});
+    }
+});
+
 export default doctorRouter;
--- back-end/service/doctor.service.ts
+++ back-end/service/doctor.service.ts
@@ -3,6 +3,15 @@ import doctorDb from "../repository/doctor.db";
 
 const getAllDoctors = (): Promise<Doctor[]> => doctorDb.getAllDoctors();
 
+const getDoctorById = async ({ id }: { id: number }): Promise<Doctor> => {
+    const doctor = await doctorDb.getDoctorById({ id });
+    if (!doctor) {
+        throw new Error(`Doctor with id ${id} not found`);
+    }
+    return doctor;
+};
+
 export default { 
     getAllDoctors,
+    getDoctorById,
 };
