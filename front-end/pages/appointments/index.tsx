2936c3082c7f60703906c54e61629f37385580fc
Furquan Mobeen <furquan.mobeen@student.ucll.be>
2024-12-19 22:06:58 +0100

    updated some stuff in front end

--- front-end/pages/appointments/index.tsx
+++ front-end/pages/appointments/index.tsx
@@ -12,7 +12,7 @@ const Appointments: React.FC = () => {
 
     const [appointments, setAppointments] = useState<Appointment[]>([]);
     const [showAppointmentForm, setShowAppointmentForm] = useState<boolean>(false);
-    const [error, setError] = useState<string>();
+    const [isAuthorized, setIsAuthorized] = useState<boolean>(true);
 
     useEffect(() => {
         const interval = setInterval(() => {
@@ -23,19 +23,16 @@ const Appointments: React.FC = () => {
       }, []);
 
     const getAllAppointments = async () => {
-        setError("");
         const response = await AppointmentService.getAllAppointments();
 
         if (!response.ok) {
             if (response.status === 401) {
-                setError("Unauthorized.");
-            } else {
-              setError(response.statusText);
+              setIsAuthorized(false);
             }
           } else {
             const appointmentsData = await response.json();
             setAppointments(appointmentsData);
-          }
+        }
     };
 
     const handleDelete = (id: number) => {
@@ -52,36 +49,42 @@ const Appointments: React.FC = () => {
 
     return (
         <>
-            <Head>
-                <title>Appointments</title>
-            </Head>
-            <Header />
-            <main className="d-flex flex-column justify-content-center align-items-center">
-                <h1>Appointments</h1>
-                {error && <div className="text-red-800">{error}</div>}
-                <section>
-                    <h2>Appointments overview</h2>
-                </section>
+          <Head>
+            <title>Appointments</title>
+          </Head>
+          <Header />
+          <main className="d-flex flex-column justify-content-center align-items-center">
+            <h1>Appointments</h1>
+            {!isAuthorized ? (
+                <div className="text-center text-red-800">
+                <h2>Appointments overview</h2>
+                <p>Unauthorized to access this page.</p>
+                {/* <p>You do not have permission to view this page.</p> */}
+              </div>
+            ) : (
+                  <>
+                    <section>
+                        <h2>Appointments overview</h2>
+                    </section>
 
-                {appointments && (
-                    <AppointmentOverviewTable appointments={appointments} deleteAppointment={handleDelete}></AppointmentOverviewTable>
+                    {appointments && (
+                        <AppointmentOverviewTable appointments={appointments} deleteAppointment={handleDelete}></AppointmentOverviewTable>
+                    )}
+
+                    <button
+                      onClick={() => setShowAppointmentForm(!showAppointmentForm)}  
+                    >
+                      Make Appointment
+                    </button>
+
+                    {showAppointmentForm && (
+                      <div className="w-full max-w-md">
+                        <MakeAppointment onAppointmentCreated={handleAppointmentCreated}></MakeAppointment>
+                      </div>
+                    )}
+                  </>
                 )}
-
-
-                <button
-                    onClick={() => setShowAppointmentForm(!showAppointmentForm)}
-                    
-                >
-                  Make Appointment
-                </button>
-
-                {showAppointmentForm && (
-                  <div className="w-full max-w-md">
-                    <MakeAppointment onAppointmentCreated={handleAppointmentCreated}></MakeAppointment>
-                  </div>
-              )}
-
-            </main>
+          </main>
         </>
         );
     };
--- front-end/services/AppointmentService.ts
+++ front-end/services/AppointmentService.ts
@@ -15,22 +15,30 @@ const getAllAppointments = async () => {
 };
 
 const deleteAppointment = (id: number) => {
+  const storedUser = sessionStorage.getItem("loggedInUser");
+  const token = storedUser ? JSON.parse(storedUser).token : null;
   const apiUrl = `${process.env.NEXT_PUBLIC_API_URL}/appointments`;
 
   return fetch(`${apiUrl}/${id}`, {
     method: 'DELETE',
     headers: {
       'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+
     },
   });
 };
 
 const makeAppointment = (appointment: Appointment) => {
+  const storedUser = sessionStorage.getItem("loggedInUser");
+  const token = storedUser ? JSON.parse(storedUser).token : null;
+
   return fetch(process.env.NEXT_PUBLIC_API_URL + "/appointments/add", {
       method: "POST",
 
       headers: {
           "Content-Type": "application/json",
+           Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(appointment),
   });
